309b38c2b4240421671a0f2a3b38b579404466c2
raman <8374679+raevilman@users.noreply.github.com>
2025-07-15 21:20:36 +0530

    Update FutureCommit formula to version 0.2.0 with macOS and Linux URLs

--- future-commit.rb
+++ future-commit.rb
@@ -1,15 +1,25 @@
+
 class FutureCommit < Formula
   desc "Use future date while making git commits"
   homepage "https://github.com/raevilman/future-commit"
-  url "https://github.com/raevilman/future-commit/releases/download/v0.1.0/future-commit.tar.gz"
-  sha256 "eb3abe8b972310fd523c3d09ae1b8e5adfeeaf961023afffd3cb5598956a47f4"
+  version "0.2.0"
   license "MIT"
 
+  on_macos do
+    url "https://github.com/raevilman/future-commit/releases/download/v0.2.0/future_commit-macos"
+    sha256 "75e4df88937c2c72e7ffabdf35d38cf453ee0038715f673f232873737ba7df29"
+  end
+
+  on_linux do
+    url "https://github.com/raevilman/future-commit/releases/download/v0.2.0/future_commit-linux"
+    sha256 "93d2ce574f900b472f5acfec0852446ce9bf54cc4ba46860a9725d175e0acf42"
+  end
+
   def install
-    bin.install "future-commit"
+    bin.install "future_commit"
   end
 
   test do
-    system "#{bin}/future-commit"
+    system "#{bin}/future_commit"
   end
 end
